8c85e16996fbd812b587e0644caabd3df1489c76
Denis Kravchuk <deniskrv@gmail.com>
2022-06-07 23:24:07 +0300

    сделал кучу компонентов для приложения

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,11 @@
+import './app-info.css'
+
+const AppInfo = () => ((
+    <div className="app-info">
+        <h1>Учет сотрудников в компании TopCompany</h1>
+        <h2>Общее число сотрудников: </h2>
+        <h2>Премию получат: </h2>
+    </div>
+))
+
+export default AppInfo
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,7 @@
+import './search-panel.css'
+
+const SearchPanel = () => ((
+    <input type="text" className='form-control search-input' placeholder='Найти сотрудника'/>
+))
+
+export default SearchPanel
