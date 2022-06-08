c03c5b87d90cbbe4764e882c202b7f38d072b748
Denis Kravchuk <deniskrv@gmail.com>
2022-06-08 19:19:00 +0300

    добавил функцию добавления записи

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -30,6 +30,21 @@ class App extends Component {
         }))
     }
 
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: uuidv4()
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
     render() {
         return (
             <div className='app'>
@@ -39,7 +54,7 @@ class App extends Component {
                     <AppFilter/>
                 </div>
                 <EmployersList data={this.state.data} onDelete={this.deleteItem}/>
-                <EmployersAddForm/>
+                <EmployersAddForm onAdd={this.addItem}/>
             </div>
         )
     }
