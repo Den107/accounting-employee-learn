bd9fd9d78315444d2f5238e1f02ccaca1db1083e
Denis Kravchuk <deniskrv@gmail.com>
2022-06-08 12:59:16 +0300

    добавил key для компонента employers list item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,9 +8,9 @@ import './app.css'
 
 function App() {
     const data = [
-        {name: 'John Smith', salary: 1000, increase: false},
-        {name: 'Sam Winchester', salary: 800, increase: false},
-        {name: 'Den Steal', salary: 1200, increase: false}
+        {name: 'John Smith', salary: 1000, increase: false, id: 1},
+        {name: 'Sam Winchester', salary: 800, increase: false, id: 2},
+        {name: 'Den Steal', salary: 1200, increase: false, id: 3}
     ]
 
     return (
